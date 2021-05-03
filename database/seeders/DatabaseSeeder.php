c6244aaeeee067a0262c93e06dfdae42991f074c
Jámbor Alexandra <ja20000907@gmail.com>
2021-05-03 22:03:52 +0200

    Added Author Model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,9 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
         $this->call(UserSeeder::class);
+        $this->call(AuthorSeeder::class);
     }
 }
