72f0065e2b8045f392e117ebdcb04c36364feb45
Jámbor Alexandra <ja20000907@gmail.com>
2021-05-11 04:46:07 +0200

    Created the navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,18 +1,21 @@
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            Cheesy Shop
-        </a>
-        <!-- TODO: Fix dropdown on small screens -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light" aria-label="Main navbar">
+    <div class="container-xl">
+        <x-nav.brand>Best Bookshop</x-nav.brand>
+        <x-nav.toggler />
 
-        <div class="collapse navbar-collapse" id="navbarContent">
-            <ul class="navbar-nav ml-auto">
-                <!-- Login / Register links -->
-                @include('components.nav.authLinks')
+        <div class="collapse navbar-collapse" id="navbarMain">
+            <ul class="navbar-nav me-5 mb-2 mb-lg-0">
+                <x-nav.link to="home" />
+                <x-nav.link to="products.index">Shop</x-nav.link>
             </ul>
+            <div class="d-inline-block me-auto">
+                <x-nav.search />
+            </div>
+            <div class="d-none d-lg-inline btn-group">
+                <livewire:cart.main />
+                <x-nav.auth />
+            </div>
+
         </div>
     </div>
 </nav>
