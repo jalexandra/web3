946de645c0e334f0a8ef3c868561b5015111f611
Jámbor Alexandra <ja20000907@gmail.com>
2021-05-16 15:38:27 +0200

    Fixed bug with form.js

--- public/js/form.js
+++ public/js/form.js
@@ -1,8 +1,19 @@
-$("#btn_filter").click(function(){
-    $("input").each(function(){
-        if($(this).val() == '') {
-            $(this).remove();
-        }
-    });
-    $("#form_id").submit();
+document.addEventListener("DOMContentLoaded", function(){
+    setInputsDisabled(false) //Enable all inputs in case the user clicked back
 });
+
+function handleEmptyInputs(){
+    setInputsDisabled(true)
+    return true
+}
+
+/** @description This function disables all empty input, therefore they won't appear in the URL */
+function setInputsDisabled(isDisabled){
+    let form = document.getElementById('form_filter')
+    let inputs = form.getElementsByTagName('input')
+    for (let i = 0; i < inputs.length; i++){
+        if (inputs[i].value === ''){
+            inputs[i].disabled = isDisabled
+        }
+    }
+}
