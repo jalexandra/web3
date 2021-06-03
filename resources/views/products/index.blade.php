944fa64f9197be6937267cb39cef6a548dfd9748
Jámbor Alexandra <ja20000907@gmail.com>
2021-06-03 19:35:42 +0200

    Moved Filter and Product cards on Product Index to their own components

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -18,31 +18,10 @@
     </div>
     <div class="row">
         <div class="col-2">
-            <form action="{{ route('products.index') }}" id="form_filter" onsubmit="handleEmptyInputs()">
-                <x-card header="Filter">
-                    <div class="input-group my-2">
-                        <input type="text" class="form-control" id="search_filter" name="search" aria-label="Search" placeholder="Search in name" value="{{ $searchTerm ?? '' }}"/>
-                    </div>
-                    <h6>Price:</h6>
-                    <div class="input-group my-2">
-                        <input type="number" class="form-control" name="price_from" aria-label="Price Minimum" placeholder="Min" value="{{ $priceMin ?? '' }}"/> -
-                        <input type="number" class="form-control" name="price_to" aria-label="Price Maximum" placeholder="Max" value="{{ $priceMax ?? '' }}"/>
-                    </div>
-
-                    <h6>In Category:</h6>
-                    @foreach($categories as $category)
-                        <x-form.checkbox name="chk_categories[]" :value="$category->id">{{ $category->name }}</x-form.checkbox>
-                    @endforeach
-                    <div class="input-group my-2">
-                        <button id="btn_filter" class="btn btn-outline-primary">Filter...</button>
-                    </div>
-                </x-card>
-            </form>
+            <x-products.filter :categories="$categories" />
         </div>
         <div class="col-10">
-            @foreach($books as $book)
-                {{ $book->title }},
-            @endforeach
+            @each('products.card', $books, 'book', 'products.empty')
         </div>
     </div>
 @endsection
