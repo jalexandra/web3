717842e16230f172b9ea5f1cd475e43778837a3b
Jámbor Alexandra <ja20000907@gmail.com>
2021-06-12 15:34:17 +0200

    Added User Index to navigation

--- resources/views/components/nav/link.blade.php
+++ resources/views/components/nav/link.blade.php
@@ -1,3 +1,10 @@
-<li class="nav-item">
-    <a class="nav-link @if(\Illuminate\Support\Facades\Route::is($is ?? $to)) active @endif" aria-current="page" href="{{ $directLink ?? route($to) }}">{{ $slot->isNotEmpty() ? $slot : \Illuminate\Support\Str::title($to) }} </a>
-</li>
+@php
+    $bouncer = $bouncer ?? false;
+@endphp
+@if (!$bouncer || (Auth::check() && Auth::user()->can($to, $bouncer)))
+    <li class="nav-item">
+        <a class="nav-link @if(\Illuminate\Support\Facades\Route::is($is ?? $to)) active @endif" aria-current="page" href="{{ $directLink ?? route($to) }}">
+            {{ $slot->isNotEmpty() ? $slot : \Illuminate\Support\Str::title($to) }}
+        </a>
+    </li>
+@endif
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,8 +5,14 @@
 
         <div class="collapse navbar-collapse" id="navbarMain">
             <ul class="navbar-nav me-5 mb-2 mb-lg-0">
-                <x-nav.link to="home" />
+{{--                <x-nav.link to="home" />--}}
                 <x-nav.link to="book.index">Shop</x-nav.link>
+                @if(Auth::user()?->roles()->count() > 0)
+                    <li class="nav-item">
+                        <a href="#" class="nav-link disabled">|</a>
+                    </li>
+                    <x-nav.link to="user.index" :bouncer="\App\Models\User::class">Users</x-nav.link>
+                @endif
             </ul>
             <div class="d-inline-block me-auto">
                 <x-nav.search />
